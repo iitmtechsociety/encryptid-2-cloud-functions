19c9489b200d1a3c80c2bb75b2bbd4f7d0204e2b
Krishaay Jois <krishaayjois@gmail.com>
2023-12-09 23:08:10 +0530

    Fixed shit

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 const functions = require('firebase-functions');
 
 exports.beforeCreate = functions.auth.user().beforeCreate((user,_)=>{
+    console.log("Email -> "+user.email);
     if(user.email === undefined){
+        console.log("Email is undefined");
         throw new functions.https.HttpsError('invalid-argument','The email address is missing');
     }
-    if(!(user.email.toLowerCase().endsWith('@study.iitm.ac.in'))){
+    if(!(user.email.toLowerCase().endsWith('study.iitm.ac.in'))){
+        console.log("Email is not a valid IITM email address");
         throw new functions.https.HttpsError('invalid-argument','The email address is not a valid IITM email address');
     }
+    return null;
 });
